efb5a0118139dcbce514e351aeca6ecb705a0ed4
Chriss972 <chriss.bellay@gmail.com>
2021-01-07 16:30:14 +0100

    Ajout header_view et footer_view dans un dossier

---
+++ templates/footer_view.php
@@ -0,0 +1,16 @@
+      <!-- Footer -->
+      <footer>
+        <section id="footer">
+            <div class="text-center">
+                <a class="link" href="#">Term of use</a>
+            </div>
+        </section>
+      </footer>
+      <!-- End Footer -->
+
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+      <link rel="stylesheet" href="<?php echo base_url('js/script.js')?>"></script>
+
+  </body>
+</html>
---
+++ templates/header_view.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Projet OME</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="public/css/style.css">
+    <link rel="stylesheet" type="text/css" href="../public/css/style.css">
+ 
+  </head>
+
+  <body>
+      <!-- Header -->
+      <header>
+          <!-- Section -->
+            <section id="header">
+                <!-- Navigation -->
+                <nav class="navbar navbar-default navbar-fixed-top">
+                    <div class="container">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                            </button>
+                            <a class="navbar-brand" href="#">One Million Euros</a>
+                        </div>
+                        <div class="collapse navbar-collapse" id="myNavbar">
+                            <ul class="nav navbar-nav navbar-right">
+                                <li class="nav-item">
+                                    <a class="nav-link tm-nav-link"  href="<?php echo base_url("/onemillioneuros")?>">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link tm-nav-link"  href="<?php echo base_url("/onemillioneuros/login")?>">Log In</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                <!-- End Navigation -->
+            </section>
+            <!-- End Section -->
+      </header>
+      <!-- End Header -->
+
+
