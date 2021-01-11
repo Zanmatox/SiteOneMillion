5b1b91682f6790fb665c33b823eee7437c338665
Zanmatox <cheungkevin@hotmail.fr>
2021-01-11 18:53:21 +0100

    Update pages de KB dans le CI4

---
+++ app/Controllers/AssosPage.php
@@ -0,0 +1,12 @@
+<?php namespace App\Controllers;
+
+class AssosPage extends BaseController
+{
+	public function index()
+	{
+		return view('Assospage');
+	}
+
+	//--------------------------------------------------------------------
+
+}
---
+++ app/Controllers/ContactController.php
@@ -0,0 +1,39 @@
+<?php 
+namespace App\Controllers;
+use App\Models\ContactModel;
+use CodeIgniter\Controller;
+
+class ContactController extends BaseController
+{
+
+    public function create() {
+        return view('Ompage_view');
+    }
+ 
+    public function form() {
+        helper(['form', 'url']);
+        
+        $input = $this->validate([
+            'Nom' => 'required|min_length[2]',
+            'Email' => 'required|valid_email',
+            'Message' => 'required|min_length[3]'
+        ]);
+
+        $contactModel = new ContactModel();
+ 
+        if (!$input) {
+            echo view('Ompage_view', [
+                'validation' => $this->validator
+            ]);
+        } else {
+            $contactModel->save([
+                'Nom' => $this->request->getVar('Nom'),
+                'Email'  => $this->request->getVar('Email'),
+                'Message'  => $this->request->getVar('Message'),
+            ]);          
+
+            return $this->response->redirect(base_url('/index.php/ContactController/create'));
+        }
+    }
+
+}
