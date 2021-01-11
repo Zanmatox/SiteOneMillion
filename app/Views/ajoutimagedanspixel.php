0e580a620d0ee1c5571184148ad49ba603b91335
Alexlutteur <alexfc.professionnel@gmail.com>
2021-01-11 01:40:04 +0100

    ajout ajouimagedanspixel.php alex

---
+++ app/Views/ajoutimagedanspixel.php
@@ -0,0 +1,68 @@
+<div class="container" style="text-align:center">
+	<div class="row">
+		<div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">				+
+            <h1>
+                Ajoutez vos images ici
+            </h1>
+                <div class="additional">
+                    Obtenir des liens permanents pour Facebook, Twitter, forums de discussion et blogs
+                </div>
+                    <div id="uploader">
+	                    <div class="container">
+	                        <form name="uploadForm" id="uploadForm" method="post" action="/json/rr" enctype="multipart/form-data">
+		                        <div class="row" style="margin-bottom:0">
+			                        <div class="col-sm-12" id="uploadControls">
+				                        <input type="hidden" name="source" value="computer">
+                                        <input type="hidden" name="ui" id="ui" value="[24,1600,900,&quot;true&quot;,&quot;&quot;,&quot;&quot;,&quot;11/01/2021 à 01:09:54&quot;]">
+                                        <input type="hidden" name="session_upload" id="session_upload" value="1610323794208">
+                                        <input type="hidden" name="gallery" id="gallery" value="">					
+                                        <div class="row">
+                    					    <div class="col-sm-12 align-center" style="line-height:135%">
+						                        <span class="width100 btn btn-lg dz-clickable" style="margin-top:10px" id="uploadFile">
+                                                    <i class="fa fa-lg fa-folder-open-o">
+                                                    
+                                                    </i> 
+                                                    Choisissez images
+                                                </span>
+                                                <p class="align-center">
+                                                    Cliquez pour choisir, copier et coller ou glisser-déposer des fichiers n'importe où
+                                                </p>
+                                                <p>	
+
+                                                </p>
+                                            </div>
+				                        </div>
+			                        </div>
+                                    <div id="uploadQueue" class="uploaddrop col-sm-12 hidden" style="padding:10px; height:auto; overflow-y:auto;">
+                                    </div>
+		                        </div>
+			                    <div class="row hidden" style="text-align: center; margin-top:10px; margin-bottom:10px">
+                                    <div class="col-md-4">
+                                    </div>
+                                    <div class="col-md-4">
+                                        <a href="javascript:void(0)" class="btn btn-lg" id="proceedbtn">
+                                            Procéder
+                                        </a>
+                                    </div>
+                                    <div class="col-md-4">
+                                    </div>
+                                </div>
+		                    </form>
+	                    </div>
+                    </div>
+		        </div>
+	        </div>
+	        <div class="row">
+                <div style="margin-top: 10px;text-align: center">
+                    En hébergeant des images sur notre site, vous acceptez le 
+                    <a href="/terms">
+                        Conditions d'utilisation
+                    </a>
+                        .
+                </div>
+            </div>
+	    </div>
+    </div>
+</div>
+
+        
