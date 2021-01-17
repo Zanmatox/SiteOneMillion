59bbb861017c0d2e1b1e0812f4ef0e20dece3dbe
Alexlutteur <alexfc.professionnel@gmail.com>
2021-01-17 20:45:51 +0100

    construction controller Empxl maj ajoutimgdspxl et empxl

--- app/Controllers/EmplacementPixel.php
+++ app/Controllers/EmplacementPixel.php
@@ -1,32 +1,32 @@
 <?php namespace App\Controllers;
 
-class AchatImageDansPixel extends BaseController
+class EmplacementPixel extends BaseController
 {
         function index()
         {
             echo view('templates/header_view');
-            echo view('ajoutimagedanspixel_view');
+            echo view('emplacementpixel_view');
             echo view('templates/footer_view');
         }
         
         function insert()
         {
             echo view('templates/header_view');
-            echo view('emplacementpixel_view');
+            echo view('accueil');
             echo view('templates/footer_view');
         }
 
         function infos()
         {
             echo view('templates/header_view');
-            echo view('ajoutimagedanspixel_view'); 
+            echo view('emplacementpixel_view'); 
             echo view('templates/footer_view');
         }
 
         function confirm()
         {
             echo view('templates/header_view');
-            echo view('emplacementpixel_view'); 
+            echo view('accueil'); 
             echo view('templates/footer_view');
         }
 
--- app/Views/emplacementpixel_view.php
+++ app/Views/emplacementpixel_view.php
@@ -1,101 +1,26 @@
 
 
-		<div id="d" style="position: absolute; z-index: 99; left: 103px; top: 962px; border: 1px solid rgb(255, 0, 0); display: none; background: rgb(255, 255, 255);">
-			<span id="xcoord">
-				Ces pixels ne sont pas disponibles, désolé (1,
-			</span>
-			<span id="ycoord">
-				85)
-			</span>
-		</div>
-
-<<<<<<< HEAD
-		<center>
-			<table style="position:relative; LEFT: 0; TOP: 0; z-index: 0;" border="0" bgcolor="#e1e1e1" cellpadding="0" cellspacing="0" width="1002">
-				<tbody>
-					<tr>
-						<td id="tablebreak" width="1">
-
-						</td>
-						<td bgcolor="#d9ab22" background="./public/assets/img/navbg.gif" height="20">
-
-
-							<!-- navbar style="color: #ffffff;" -->
-
-							<table border="0" cellpadding="0" cellspacing="0" width="1000" height="20">
-								<tbody>
-									<tr>
-										<td>
-											<a class="nav" href="http://localhost:8080/SiteOneMillion/">Accueil</a> &nbsp;|&nbsp; 
-											<a class="nav" href="http://localhost:8080/SiteOneMillion/achatpixel.php">Achat de Pixels</a> &nbsp;|&nbsp; 
-											<a class="nav" href="http://localhost:8080/SiteOneMillion/infos">Infos</a> &nbsp;|&nbsp;
-											<a class="nav" href="http://localhost:8080/SiteOneMillion/pixellist.php">Liste des Pixels</a> &nbsp;|&nbsp;
-											<a class="nav" href="http://localhost:8080/SiteOneMillion/Contact">Contactez nous</a>
-										</td>
-									</tr>
-								</tbody>
-							</table>
-
-							<!-- /navbar -->
-
-
-						</td>
-						<td id="tablebreak" width="1">
-
-						</td>
-					</tr>
-					<tr>
-						<td id="tablebreak" colspan="3">
-
-						</td>
-					</tr>
-						<!-- end header file -->
-					<tr>
-						<td id="tablebreak" width="1">
-
-						</td>
-						<td width="1000" height="1000" background="./public/assets/img/bg10.gif" valign="top">
-							<div id="f" style="display:none;font-size:0px;margin:0px;padding:0px;border: #0000ff 2px solid;position: absolute;width:1px;height:1px;z-index:2;">
-							
-							</div>
-
-							<div id="pixels" style="z-index:1">
-								<map name="Map" id="Map">
-									<area onmouseover="d(this)" onmouseout="e(this)" shape="rect" coords="630,310,640,320" href="http://www.getpixel.net/" title="getpixel.net, stock photography">
-									
-								</map><img name="twee" id="twee" src="./public/assets/img/image-map.png" width="1000" height="1000" border="0" ismap="" usemap="#Map" style="position: absolute; top: 0px; left: 0px; border: none">
-								<img name="een" id="een" src="./public/assets/img/image-map(1).png" width="2000" height="2000" border="0" style="position: absolute; top: 0px; left: 0px; clip: rect(0px, 100px, 100px, 0px); border: black 2px solid; background: url(bg20.gif); display:none">
-								<img src="./public/assets/img/neg.gif" name="neg" id="neg" width="1000" height="1000" style="position:absolute;top:0px;left:0px;display:none">
-							</div>
-						</td>
-						<td id="tablebreak" width="1">
-
-						</td>
-					</tr>			
-				</tbody>
-			</table>
-		</center>
-=======
-		<div>
+		<body>	
 			<div id="d" style="position: absolute; z-index: 99; left: 103px; top: 962px; border: 1px solid rgb(255, 0, 0); display: none; background: rgb(255, 255, 255);">
 				<span id="xcoord">
-					Ces pixels ne sont pas disponibles, désolé (1,
+					These pixels are not available, sorry (1,
 				</span>
 				<span id="ycoord">
 					85)
 				</span>
 			</div>
 
+
 			<div>
 				<center>
 					<table style="position:relative; LEFT: 0; TOP: 0; z-index: 0;" border="0" bgcolor="#e1e1e1" cellpadding="0" cellspacing="0" width="1002">
-						<tbody>							
+						<tbody >							
 							<tr>
 								<td id="tablebreak" width="1">
 
 								</td>
-								<td width="1000" height="1000" background="./public/assets/img/bg10.gif" valign="top">
-									<div id="f" style="display:none;font-size:0px;margin:0px;padding:0px;border: #0000ff 2px solid;position: absolute;width:1px;height:1px;z-index:2;">
+								<td width="1000" height="1000" background="./public/assets/img/bg10.gif" valign="top" style="position: absolute; top: 100px; left: 0px; border: none">
+									<div id="f" style="display:none;font-size:0px;margin:0px;padding:0px;border: #0000ff 2px solid;position:absolute;width:1px;height:1px;z-index:2;">
 									
 									</div>
 
@@ -104,9 +29,9 @@
 											<area onmouseover="d(this)" onmouseout="e(this)" shape="rect" coords="0,1000,0,1000" href="https://www.afpa.fr/" title="afpa.fr, centre de formation">
 											
 										</map>
-										<img src="./public/assets/img/image-map.png" width="1000" height="1000" border="0" ismap="" usemap="#Map" style="position: absolute; top: 0px; left: 0px; border: none">
-										<img src="./public/assets/img/image-map(1).png" width="2000" height="2000" border="0" style="position: absolute; top: 0px; left: 0px; clip: rect(0px, 100px, 100px, 0px); border: black 2px solid; background: url(bg20.gif); display:none">
-										<img src="./public/assets/img/neg.gif" name="neg" id="neg" width="1000" height="1000" style="position:absolute;top:0px;left:0px;display:none">
+										<img src="public/assets/img/image-map.png" width="1000" height="1000" _border="0" ismap="" usemap="#Map" style="position: absolute; top: 0px; left: 0px; border: none">
+										<img src="public/assets/img/image-map(1).png" width="2000" height="2000" _border="0" style="position: absolute; top: 0px; left: 0px; clip: rect(0px, 100px, 100px, 0px); border: black 2px solid; background: url(bg20.gif); display:none">
+										<img src="public/assets/img/neg.gif" name="neg" id="neg" width="1000" height="1000" _border="0" style="position:absolute;top:0px;left:0px;display:none">
 									</div>
 								</td>
 								<td id="tablebreak" width="1">
@@ -115,7 +40,7 @@
 							</tr>
 								<!-- end actual page content -->
 							<tr>
-								<td id="note" height="24" colspan="3">
+								<td id="note" height="24" colspan="3" style="position: absolute; top: 1100px; left: 0px; border: none">
 									One Million euro Homepage © 2021 CDA. 
 									Tout droits réservés. 
 									Nous ne sommes pas responsables du contenu des sites externes. 
@@ -127,7 +52,8 @@
 					</table>
 				</center>
 			</div>
-		</div>
+		</body>
+		
 
->>>>>>> fe689cf1b2800186a50181f71f2abdb03f845df6
+
 		
