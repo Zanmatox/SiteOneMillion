9a1290a28e6cdb569a5701123397cf1308b43718
Alexlutteur <alexfc.professionnel@gmail.com>
2021-01-20 11:33:23 +0100

    Maj empxlview alex

--- app/Views/emplacementpixel_view.php
+++ app/Views/emplacementpixel_view.php
@@ -10,18 +10,18 @@
 
 			<div>
 				<center>
-					<table style="position:relative; LEFT: 0; TOP: 0; z-index: 0;" border="0" bgcolor="#e1e1e1" cellpadding="0" cellspacing="0" width="1002">
+					<table style="position:relative; LEFT: 0; TOP: 100px; BOTTOM: 100px; z-index: 0;" border="0" bgcolor="#e1e1e1" cellpadding="0" cellspacing="0" width="1002">
 						<tbody>							
 							<tr>
-								<td id="tablebreak" width="1">
+								<td id="tablebreak" width="1" >
 
 								</td>
 								<td width="1000" height="1000" background="./public/assets/img/bg10.gif" valign="top">
-									<div id="f" style="display:none;font-size:0px;margin:0px;padding:0px;border: #0000ff 2px solid;position: absolute;width:1px;height:1px;z-index:2;">
+									<div id="f" style="display:none;font-size:0px;margin:0px;padding:0px;border: #0000ff 2px solid;position: absolute; top: 100px ;width:1px;height:1px;z-index:2;">
 									
 									</div>
 
-									<div id="pixels" style="z-index:1">
+									<div id="pixels" style="z-index:1; position: absolute; top: 0px; left: 0px; border: none">
 										<map name="Map" id="Map">
 											<area onmouseover="d(this)" onmouseout="e(this)" shape="rect" coords="0,1000,0,1000" href="https://www.afpa.fr/" title="afpa.fr, centre de formation">
 											
@@ -37,7 +37,7 @@
 							</tr>
 								<!-- end actual page content -->
 							<tr>
-								<td id="note" height="24" colspan="3">
+								<td id="note" height="24" colspan="3" style="position: absolute; top: 1000px; left: 0px; border: none">
 									One Million euro Homepage © 2021 CDA. 
 									Tout droits réservés. 
 									Nous ne sommes pas responsables du contenu des sites externes. 
@@ -50,4 +50,11 @@
 				</center>
 			</div>
 		</div>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
+		<br>
 		
