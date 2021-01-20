03d1759349ddc348d641c459a122e8394b765b51
Alexlutteur <alexfc.professionnel@gmail.com>
2021-01-20 14:49:29 +0100

    Modif ajoutimgdspixel alex

--- app/Controllers/Ajoutimagedanspixel.php
+++ app/Controllers/Ajoutimagedanspixel.php
@@ -1,7 +1,8 @@
 <?php namespace App\Controllers;
 
-class Achat extends BaseController
+class AjoutImageDansPixel extends BaseController
 {
+    private $session;
         /*function index()
         {
             echo view('templates/header_view');
